dc569133ec45abf3fa272b76a9ce67b27bbfc673
ivnkwok <ivnkwok@gmail.com>
2024-06-17 21:02:19 -0700

    Update baerryModule.js

--- client/src/components/baerryModule.js
+++ client/src/components/baerryModule.js
@@ -13,15 +13,13 @@ export default function BaerryModule({foods}){
 
 function createModule(food) {
     return (
-    <div className="border-wood border-4 w-[100%] h-28 bg-[url('./assets/moduleBG.png')] bg-bottom bg-left bg-contain">
-            <img className="float-left" src={woerm}/>
-            <div className="float-right h-[100%]">
-                <img className="w-40 relative top-30 float-left" src={basicBaer}/>
-                <div className='border-wood border-l-4 float-right h-[100%] min-w-fit font-default bg-[#904B19] text-center'>
-                    <h1 className='text-2xl'>{food.title}</h1>
-                    <h2 className='text-base'>{food.levelInfo}</h2>
-                    <p className='text-sm'>{food.description}</p>
-                </div>
+    <div className="flex justify-end border-wood border-4 h-28 bg-[url('./assets/moduleBG.png')] bg-bottom bg-left bg-contain">
+            <img className="" src={woerm}/>
+            <img className="ml-auto" src={basicBaer}/>
+            <div className='flex flex-col shrink-1 border-wood border-l-4 h-[100%] font-default bg-[#904B19] text-center'>
+                <h1 className='text-2xl'>{food.title}</h1>
+                <h2 className='text-base'>{food.levelInfo}</h2>
+                <p className='text-sm'>{food.description}</p>
             </div>
         </div>
     );
