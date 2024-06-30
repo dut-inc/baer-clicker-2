ffd17b8dbc234ac151d677a6ca4a7b22dda86b7e
Ivan Kwok <ivnkwok@gmail.com>
2024-06-30 11:31:38 -0700

    made hunting title sticky

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -142,8 +142,8 @@ function App() {
             <div ref = {clickRef}>{data.clicks}</div>
             <img class="object-contain h-48 w-96 bg-white shadow rounded-lg" src="link" alt="dynamic button" onClick={() => handleClick()}/>
           </div>
-          <div className='flex flex-col border-4 border-leavesdark bg-wood h-screen overflow-y-auto overflow-hidden'>
-            <div className='border-4 border-leavesdark text-center bg-[#779025] font-default text-4xl'>
+          <div className='flex flex-col border-4 border-leavesdark bg-wood h-screen overflow-y-auto'>
+            <div className='border-4 border-leavesdark text-center bg-[#779025] font-default text-4xl sticky top-0 z-10'>
               Hunting
             </div>
            <UpgradeModule foods={foodList} data={data} setData={setData}/>
