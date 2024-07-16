15fd41d4e14b41146006c9cd512f2bce5a6441f6
Jkites <99958936+Jkites@users.noreply.github.com>
2024-07-15 23:43:44 -0700

    added upgrades schema

--- api/app.mjs
+++ api/app.mjs
@@ -28,6 +28,7 @@ app.use(cors(
 
 const User = mongoose.model('User')
 const Clicks = mongoose.model('Clicks')
+const Upgrades = mongoose.model('Upgrades')
 
 app.use(session({
     secret: "BAERBALEJOEIFJOSIEJF",
@@ -59,33 +60,56 @@ app.get('/', async (req, res) => {
     if (!userClicks) {
         const newClicks = new Clicks({
             clicks: 0,
-            woerms: 0,
+            woermCurrencies: 0,
             spiritBaers: 0,
             rainbowTrouts: 0,
             user: foundUser._id
         })
-        newClicks.save()
-        .then(() => {
-            res.json({ 
-                clicks: 0,
-                woerms: 0,
-                spiritBaers: 0,
-                rainbowTrouts: 0
-             })
-        }).catch((err) => {
-            console.log(err)
+        await newClicks.save()
+
+        const newUpgrades = new Upgrades({
+            woermUpgrades: 0,
+            baerrys: 0,
+            paenutz: 0,
+            gyatchs: 0,
+            nickaels: 0,
+            saelmons: 0,
+            uraeniums: 0,
+        })
+        await newUpgrades.save()
+        res.json({
+            clicks: 0,
+            woermCurrencies: 0,
+            spiritBaers: 0,
+            rainbowTrouts: 0,
+            woermUpgrades: 0,
+            baerrys: 0,
+            paenutz: 0,
+            gyatchs: 0,
+            nickaels: 0,
+            saelmons: 0,
+            uraeniums: 0
         })
 
     } else {
+        const userUpgrades = await Upgrades.findOne({ user: foundUser._id })
         res.json({ 
             clicks: userClicks.clicks,
-            woerms: userClicks.woerms,
+            woermsCurrencies: userClicks.woerms,
             spiritBaers: userClicks.spiritBaers,
-            rainbowTrouts: userClicks.rainbowTrouts
+            rainbowTrouts: userClicks.rainbowTrouts,
+            woermUpgrades: userUpgrades.woermUpgrades,
+            baerrys: userUpgrades.baerrys,
+            paenutz: userUpgrades.paenutz,
+            gyatchs: userUpgrades.gyatchs,
+            nickaels: userUpgrades.nickaels,
+            saelmons: userUpgrades.saelmons,
+            uraeniums: userUpgrades.uraeniums
         })
     }
 })
 
+//TODO: update still needs to be changed
 app.post('/click', async (req, res) => {
     const newClicks = parseInt(req.body.clicks)
     // const newWoerms = parseInt(req.body.woerms)
--- api/db.mjs
+++ api/db.mjs
@@ -8,15 +8,28 @@ const userSchema = new mongoose.Schema({
     username: {type: String, required: true}
 })
 
+//TODO: refactor clicks to calories
 const clickSchema = new mongoose.Schema({
     clicks: { type: Number, required: true },
-    woerms: { type: Number, required: true },
+    woermCurrencies: { type: Number, required: true },
     spiritBaers: { type: Number, required: true },
     rainbowTrouts: { type: Number, required: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 })
 
+//amount of upgrades
+const upgradeSchema = new mongoose.Schema({
+    woermUpgrades: { type: Number, required: true },
+    baerrys: { type: Number, required: true },
+    paenutz: { type: Number, required: true },
+    gyatchs: { type: Number, required: true },
+    nickaels: { type: Number, required: true },
+    saelmons: { type: Number, required: true },
+    uraeniums: { type: Number, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+})
 userSchema.plugin(passportLocalMongoose)
 
 mongoose.model('User', userSchema)
 mongoose.model('Clicks', clickSchema)
+mongoose.model('Upgrades', upgradeSchema)
